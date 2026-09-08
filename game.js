a80be8e2ee480f2dacba00e9c8263465a49faec3
Thomas Lovin <thomasrlovin@gmail.com>
2026-09-08 18:01:20 -0400

    Game Over

--- game.js
+++ game.js
@@ -43,7 +43,6 @@ function nextSequence(){
     var randomNumber = Math.floor(Math.random() * 4);
     var randomChosenColor = buttonColours[randomNumber];
     gamePattern.push(randomChosenColor);
-    console.log(gamePattern);
 
     animatePress(randomChosenColor)
     playSound(randomChosenColor);
@@ -52,6 +51,24 @@ function nextSequence(){
     $("h1").text("level " + level);
 }
 
+function checkAnswer(currentLevel){
+    if(userClickedPattern[currentLevel] === gamePattern[currentLevel]){
+
+        if (userClickedPattern.length == gamePattern.length){
+            userClickedPattern = [];
+            setTimeout(nextSequence, 1000);
+        }
+    }
+    else{
+        var audio = new Audio("./sounds/wrong.mp3");
+        audio.play();
+        $("body").addClass("game-over");
+        setTimeout(function(){
+            $("body").removeClass("game-over")
+        }, 200);
+        $("h1").text("Game Over, Press Any Key to Restart");
+    }
+}
 
 $(".btn").on("click", function(){
     var userChosenColor = this.id;
@@ -60,6 +77,7 @@ $(".btn").on("click", function(){
     animatePress(this.id)
     playSound(this.id);
 
+    checkAnswer(userClickedPattern.length -1);
     
 });
 
